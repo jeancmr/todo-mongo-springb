157d3848c39a5152c0f8acf61378dfb264a86dcd
jeancmr <jean.madiedo08@gmail.com>
2026-05-17 19:07:00 -0500

    feat: add timestamps

--- src/main/java/com/jean/todomongo/model/Todo.java
+++ src/main/java/com/jean/todomongo/model/Todo.java
@@ -1,8 +1,12 @@
 package com.jean.todomongo.model;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
 
 @Document(collection = "todos")
 @Getter
@@ -18,4 +22,10 @@ public class Todo {
     private String title;
 
     private boolean completed;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
 }
